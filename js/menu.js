26079a85122950f266040887b723d0cdf536a038
Felippe <nascimento.conceito@gmail.com>
2023-08-12 23:39:49 -0300

    Estrutura e Estilo Básico do Menu Lateral

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=>
+    item.addEventListener('click', selectLink)
+)
