e738151149ff9ce45554a002276d6b733755d700
Felippe <nascimento.conceito@gmail.com>
2023-09-04 23:23:56 -0300

    construindo api

--- js/cliente.js
+++ js/cliente.js
@@ -1,121 +1,68 @@
-// Validate CEP Input
-cepInput.addEventListener("keypress", (e) => {
-    const onlyNumbers = /[0-9]|\./;
-    const key = String.fromCharCode(e.keyCode);
-  
-    console.log(key);
-  
-    console.log(onlyNumbers.test(key));
-  
-    // allow only numbers
-    if (!onlyNumbers.test(key)) {
-      e.preventDefault();
-      return;
-    }
-  });
-  
-  // Evento to get address
-  cepInput.addEventListener("keyup", (e) => {
-    const inputValue = e.target.value;
-  
-    //   Check if we have a CEP
-    if (inputValue.length === 8) {
-      getAddress(inputValue);
-    }
-  });
-  
-  // Get address from API
-  const getAddress = async (cep) => {
-    toggleLoader();
-  
-    cepInput.blur();
-  
-    const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
-  
-    const response = await fetch(apiUrl);
-  
-    const data = await response.json();
-  
-    console.log(data);
-    console.log(formInputs);
-    console.log(data.erro);
-  
-    // Show error and reset form
-    if (data.erro === "true") {
-      if (!addressInput.hasAttribute("disabled")) {
-        toggleDisabled();
-      }
-  
-      addressForm.reset();
-      toggleLoader();
-      toggleMessage("CEP Inválido, tente novamente.");
-      return;
-    }
-  
-    // Activate disabled attribute if form is empty
-    if (addressInput.value === "") {
-      toggleDisabled();
-    }
-  
-    addressInput.value = data.logradouro;
-    cityInput.value = data.localidade;
-    neighborhoodInput.value = data.bairro;
-    regionInput.value = data.uf;
-  
-    toggleLoader();
-  };
-  
-  // Add or remove disable attribute
-  const toggleDisabled = () => {
-    if (regionInput.hasAttribute("disabled")) {
-      formInputs.forEach((input) => {
-        input.removeAttribute("disabled");
-      });
-    } else {
-      formInputs.forEach((input) => {
-        input.setAttribute("disabled", "disabled");
-      });
-    }
-  };
-  
-  // Show or hide loader
-  const toggleLoader = () => {
-    const fadeElement = document.querySelector("#fade");
-    const loaderElement = document.querySelector("#loader");
-  
-    fadeElement.classList.toggle("hide");
-    loaderElement.classList.toggle("hide");
-  };
-  
-  // Show or hide message
-  const toggleMessage = (msg) => {
-    const fadeElement = document.querySelector("#fade");
-    const messageElement = document.querySelector("#message");
-  
-    const messageTextElement = document.querySelector("#message p");
-  
-    messageTextElement.innerText = msg;
-  
-    fadeElement.classList.toggle("hide");
-    messageElement.classList.toggle("hide");
-  };
-  
-  // Close message modal
-  closeButton.addEventListener("click", () => toggleMessage());
-  
-  // Save address
-  addressForm.addEventListener("submit", (e) => {
+const addressForm = document.querySelector("#address-form");
+const cepInput = document.querySelector("#cep");
+const addressInput = document.querySelector("#address");
+const cityInput = document.querySelector("#city");
+const neighborhoodInput = document.querySelector("#neighborhood");
+const regionInput = document.querySelector("#region");
+const formInputs = document.querySelector("[data-input]");
+
+const closeButton = document.querySelector("#close-message");
+
+//função anomima
+cepInput.addEventListener("keypress",(e) =>{
+
+  const onlyNumbers = /[0-9]/;
+  const key = String.fromCharCode(e.keyCode);
+  console.log(e.keyCode);
+  console.log(key);
+
+  if(!onlyNumbers.test(key)){
     e.preventDefault();
+    return;
+  }
+
+});
+
+cepInput.addEventListener("keyup", (e) =>{
+
+  const inputValue = e.target.value;
+
+  if (inputValue.length === 8){
+
+    getAddress(inputValue);
+
+  }
+
+});
+
+const getAddress = async (cep) => {
+
   
+  toggleLoader();
+  cepInput.blur();
+  const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  console.log(data);
+  
+  if (data.erro === "true"){
+
+    addressForm.reset();
     toggleLoader();
+    return;
+
+  }
+
+};
+
+const toggleLoader = () => {
   
-    setTimeout(() => {
-      toggleLoader();
-  
-      toggleMessage("Endereço salvo com sucesso!");
-  
-      addressForm.reset();
-  
-      toggleDisabled();
-    }, 1000);
-  });
+  const fadeElement = document.querySelector("#fade");
+  const loaderElement = document.querySelector("#loader");
+
+  fadeElement.classList.toggle("hide");
+  loaderElement.classList.toggle("hide");
+
+
+
+}
